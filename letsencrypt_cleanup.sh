8d478eeac7368bd27ddd803c8f639058de66307d
Tri Le <thanhtrixx@gmail.com>
2019-03-02 16:04:11 +0700

    Remove all file to update new page

--- letsencrypt_cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git rm $CI_PROJECT_DIR/static/.well-known/acme-challenge/$CERTBOT_TOKEN
-git commit -m "GitLab runner - Removed certbot challenge file"
-git push https://$GITLAB_USER_LOGIN:$CERTBOT_RENEWAL_PIPELINE_GIT_TOKEN@gitlab.com/BasPH/blog.git HEAD:master
